d41cae0457ec5cef8d55e2aa74675b7b3004874b
Zulfikar <zbandhan@users.noreply.github.com>
2022-03-17 09:47:08 +0600

    Unnecessary time complexity reduced.

--- init.py
+++ init.py
@@ -9,14 +9,12 @@ sheet_info = wb['qry_Beneficiaries']
 
 #qr_gen function is responsible for generating QR codes with MEMCard formate
 def qr_gen(name = None, card = None, phone = None, nid = None, union = None, ward = None) :
-    qname   = name
     qcard   = "CARD: " + str(card)
-    qphone  = phone
     qnid    = "NID: " + str(nid)
     qunion  = "UNION: " + str(int(union)) if union != None else union
     qward   = "WARD: " + str(int(ward)) if ward != None else ward
 
-    img = qrcode.make(f"MECARD:N:{qname};ADR:{qcard};TEL:{qphone};EMAIL:{qnid};ADR:{qunion};ADR:{qward};;")
+    img = qrcode.make(f"MECARD:N:{name};ADR:{qcard};TEL:{phone};EMAIL:{qnid};ADR:{qunion};ADR:{qward};;")
     type(img)
     img.save(f"qrfiles/{card}.png")
 
